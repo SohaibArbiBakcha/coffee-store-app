e28b54b8b32c7ec08406061f2267db3298689a13
Sohaib Arbi BAKCHA <s.bakcha@atner.net>
2024-04-29 17:36:52 +0100

    add navigation fanctionality

--- lib/components/buttom_nav_bar.dart
+++ lib/components/buttom_nav_bar.dart
@@ -2,18 +2,20 @@ import 'package:flutter/material.dart';
 import 'package:google_nav_bar/google_nav_bar.dart';
 
 class ButtomNavBar extends StatelessWidget {
-  const ButtomNavBar({super.key});
+  void Function(int)? onTabChange;
+  ButtomNavBar({super.key, required this.onTabChange});
 
   @override
   Widget build(BuildContext context) {
     return GNav(
+        onTabChange: (value) => onTabChange!(value),
         color: Colors.grey[400],
         mainAxisAlignment: MainAxisAlignment.center,
         activeColor: Colors.grey[700],
         tabBackgroundColor: Colors.grey.shade300,
         tabBorderRadius: 24,
         tabActiveBorder: Border.all(color: Colors.white),
-        tabs: [
+        tabs: const [
           GButton(
             icon: Icons.home_outlined,
             text: 'Shop',
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,3 +1,6 @@
+import 'package:coffeeshop/pages/cart_page.dart';
+import 'package:coffeeshop/pages/shop_page.dart';
+
 import '../components/buttom_nav_bar.dart';
 
 import '../const.dart';
@@ -11,11 +14,31 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  // navigate bottom bar
+  int _selectedIndex = 0;
+
+  void navigateBottomBar(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  final List<Widget> _pages = [
+    //shop page
+    ShopPage(),
+
+    // cart page
+    Cartpage()
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: backgroundColor,
-      bottomNavigationBar: ButtomNavBar(),
+      bottomNavigationBar: ButtomNavBar(
+        onTabChange: (index) => navigateBottomBar(index),
+      ),
+      body: _pages[_selectedIndex],
     );
   }
 }
